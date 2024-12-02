7af73e694fa3fb1d0b7b986b84614c35f5a53b3a
said <soidroudinesaid@hotmail.fr>
2024-12-02 12:22:15 +0100

    Debeug dossier front

---
+++ front-end-react/src/App.js
@@ -0,0 +1,25 @@
+import Header from "./components/Header";
+import './assets/css/style.css';
+
+function App() {
+  return (
+    <>
+  
+    <header>
+       <Header/>
+       </header>
+       <main >
+       
+       </main>
+      
+       <footer>
+      
+       </footer>
+       
+       
+  
+       </>
+  );
+};
+
+export default App;
---
+++ front-end-react/src/components/Header.jsx
@@ -0,0 +1,65 @@
+import { Fragment } from "react";
+
+  function Header() {
+      return (
+        <Fragment className="header">
+      <nav class="flex justify-between bg-white-900 text-black w-screen">
+        <div class="px-5 xl:px-12 py-6 flex w-full items-center">
+          <a class="text-3xl font-bold font-heading" href="#"></a>
+            <h1 class="Logo"><a href="">Sport<span>C</span>ards</a></h1>
+          <ul class="hidden md:flex px-4 mx-auto font-semibold font-heading space-x-12 font-bold">
+            <li class="lienRoot text-white"><a href="#">Accueil</a></li>
+            <li class="lienRoot text-white"><a href="#">Voitures</a></li>
+            <li class="lienRoot text-white"><a href="#">Catégories</a></li>
+            <li class="lienRoot text-white"><a href="#">Contact</a></li>
+          </ul>
+        
+          <div class="hidden xl:flex items-center space-x-5 items-center">
+            <a class="hover:text-gray-200" href="#">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-4.35-4.35m2.15-5.15a7 7 0 11-14 0 7 7 0 0114 0z" />
+</svg>
+
+            </a>
+            <a class="flex items-center hover:text-gray-200" href="#">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
+                </svg>
+              <span class="flex absolute -mt-5 ml-4">
+                <span class="animate-ping absolute inline-flex h-3 w-3 rounded-full bg-pink-400 opacity-75"></span>
+                  <span class="relative inline-flex rounded-full h-3 w-3 bg-pink-500">
+                  </span>
+                </span>
+            </a>
+          
+            <a class="flex items-center hover:text-gray-200" href="#">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 hover:text-gray-200" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                </svg>
+            </a>
+            
+          </div>
+        </div>
+      
+        <a class="xl:hidden flex mr-6 items-center" href="#">
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 hover:text-gray-200" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
+          </svg>
+          <span class="flex absolute -mt-5 ml-4">
+            <span class="animate-ping absolute inline-flex h-3 w-3 rounded-full bg-pink-400 opacity-75"></span>
+            <span class="relative inline-flex rounded-full h-3 w-3 bg-pink-500">
+            </span>
+          </span>
+        </a>
+        <a class="navbar-burger self-center mr-12 xl:hidden" href="#">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 hover:text-gray-200" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+            </svg>
+        </a>
+      </nav>
+
+        </Fragment>
+      );
+    }
+    
+    export default Header;
