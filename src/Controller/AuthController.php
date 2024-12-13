8cf27cf906d21a80d23d2a2618eac9292d891ecd
said <soidroudinesaid@hotmail.fr>
2024-12-14 00:31:54 +0100

    Debauguage pour le router api/login_check en ce moment 200 ok mais token vide...

--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -3,15 +3,22 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/api', name: 'api_')]
 class AuthController extends AbstractController
 {
-    #[Route('/login_check', name: 'login_check', methods: ['POST'])]
-    public function loginCheck(): JsonResponse
+    #[Route('/api/login_check', name: 'app_security')]
+    public function loginCheck(): Response
     {
-        throw new \RuntimeException('This method should not be called directly. LexikJWTAuthenticationBundle handles the login process.');
+        // Le login_check est géré automatiquement par le LexikJWTAuthenticationBundle.
+        // Si cette méthode est appelée, il faut retourner une erreur ou indiquer que l'authentification est prise en charge ailleurs.
+        return new Response('Ce point de terminaison est géré par LexikJWTAuthenticationBundle.', Response::HTTP_BAD_REQUEST);
+    }
+
+    #[Route('/api/test', name: 'app_test')]
+    public function appTest(): Response
+    {
+        return new Response('Vous êtes connecté');
     }
 }
