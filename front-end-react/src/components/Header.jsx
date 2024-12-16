b48aa6bfde531dc0230c06155253ded43ce9fedb
said <soidroudinesaid@hotmail.fr>
2024-12-16 15:49:44 +0100

    Adapatation du header pour tout device en ajoutant un menu hamburger.

--- front-end-react/src/components/Header.jsx
+++ front-end-react/src/components/Header.jsx
@@ -1,20 +1,27 @@
-import React, { Fragment, useContext } from "react";
+import React, { Fragment, useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import { UserContext } from "./UserContext"; // Import du contexte utilisateur
 
 function Header() {
   const { user, logout } = useContext(UserContext); // Récupération de l'état utilisateur via le contexte
+  const [isMenuOpen, setIsMenuOpen] = useState(false); // État pour contrôler l'ouverture du menu
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
 
   return (
     <Fragment className="header">
       <nav className="flex justify-between bg-white-900 text-black w-screen">
-        <div className="px-5 xl:px-12 py-6 flex w-full items-center">
+        <div className="px-5 xl:px-12 py-6 flex w-full justify-between items-center">
           <h1 className="Logo">
             <Link to="/accueil">
               Sport<span>C</span>ars
             </Link>
           </h1>
-          <ul className="hidden md:flex px-4 mx-auto font-semibold font-heading space-x-12 font-bold">
+
+          {/* Menu principal pour écrans larges */}
+          <ul className="hidden 2xl:flex px-4 mx-auto font-semibold font-heading space-x-12 font-bold">
             <li className="lienRoot text-white">
               <Link to="/accueil">Accueil</Link>
             </li>
@@ -38,8 +45,8 @@ function Header() {
             )}
           </ul>
 
-          <div className="hidden xl:flex items-center space-x-5">
-            {/* Panier séparé */}
+          {/* Panier, icône login/déconnexion et icône menu pour tablette et mobile */}
+          <div className="flex items-end space-x-5">
             <Link to="/panier" className="relative hover:text-gray-500">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -57,58 +64,191 @@ function Header() {
               </svg>
             </Link>
 
-            {/* Si utilisateur non connecté */}
-            {!user.isAuthenticated && (
-              <Link to="/form-auth" className="hover:text-gray-500">
-                {/* Icone Connexion */}
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-6 w-6"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M5.121 17.804A3 3 0 017.828 16H16a3 3 0 013 3v1m-9-4h6m-6 0l3-3m-3 3l3 3"
-                  />
-                </svg>
-              </Link>
-            )}
-
-            {/* Si utilisateur connecté */}
-            {user.isAuthenticated && (
-              <>
+            {/* Icône login/déconnexion pour écrans larges */}
+            <div className="hidden 2xl:block">
+              {!user.isAuthenticated ? (
+                <Link to="/form-auth" className="hover:text-gray-500">
+                  <svg
+                    width="25px"
+                    height="25px"
+                    viewBox="0 0 16 16"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M8 7C9.65685 7 11 5.65685 11 4C11 2.34315 9.65685 1 8 1C6.34315 1 5 2.34315 5 4C5 5.65685 6.34315 7 8 7Z"
+                      fill="#000000"
+                    />
+                    <path
+                      d="M14 12C14 10.3431 12.6569 9 11 9H5C3.34315 9 2 10.3431 2 12V15H14V12Z"
+                      fill="#000000"
+                    />
+                  </svg>
+                </Link>
+              ) : (
                 <button
                   onClick={logout}
                   className="hover:text-gray-500"
                   title="Déconnexion"
                 >
-                  {/* Icone Déconnexion */}
                   <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-6 w-6"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
+                    viewBox="0 0 600 600"
+                    version="1.1"
+                    width="20"
+                    height="20"
                   >
                     <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1m-4-1V7m4 9V7"
+                      id="rect348"
+                      d="M 130 0 C 58.672245 0 0 58.672245 0 130 L 0 470 C 0 541.32776 58.672245 600 130 600 L 301.57812 600 C 367.83331 600 423.13643 549.36696 430.67188 485 L 349.43555 485 C 343.32179 505.66026 324.7036 520 301.57812 520 L 130 520 C 101.60826 520 80 498.39174 80 470 L 80 130 C 80 101.60826 101.60826 80 130 80 L 301.57812 80 C 324.7036 80 343.32179 94.339739 349.43555 115 L 430.67188 115 C 423.13642 50.633038 367.83331 0 301.57812 0 L 130 0 z"
+                    />
+                    <path
+                      id="path1073"
+                      d="m 476.86328,179.99911 a 40,40 0 0 0 -28.28516,11.71484 40,40 0 0 0 0,56.57032 l 11.71485,11.71484 H 163.72656 a 40,40 0 0 0 -40,40 40,40 0 0 0 40,40 h 296.56641 l -11.71485,11.71484 a 40,40 0 0 0 0,56.57032 40,40 0 0 0 56.57032,0 l 72.79101,-72.79102 A 40,40 0 0 0 600,299.99911 40,40 0 0 0 577.5293,264.09481 l -72.38086,-72.38086 a 40,40 0 0 0 -28.28516,-11.71484 z"
                     />
                   </svg>
                 </button>
-              </>
-            )}
+              )}
+            </div>
+
+            <button
+              className="block 2xl:hidden focus:outline-none"
+              onClick={toggleMenu}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h16M4 18h16"
+                />
+              </svg>
+            </button>
           </div>
         </div>
       </nav>
+
+      {/* Menu coulissant pour écrans étroits */}
+      {isMenuOpen && (
+        <div
+          className="fixed top-0 right-0 w-2/5 h-full bg-gray-800 text-white shadow-lg z-50 2xl:hidden"
+        >
+          <button
+            className="absolute top-4 right-4 focus:outline-none"
+            onClick={toggleMenu}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M6 18L18 6M6 6l12 12"
+              />
+            </svg>
+          </button>
+          <ul className="p-6 space-y-4">
+            <li>
+              <Link to="/accueil" className="text-white" onClick={toggleMenu}>
+                Accueil
+              </Link>
+            </li>
+            <li>
+              <Link to="/boutique" className="text-white" onClick={toggleMenu}>
+                Boutique
+              </Link>
+            </li>
+            {user.isAdmin && (
+              <>
+                <li>
+                  <Link
+                    to="/liste-vehicule"
+                    className="text-white"
+                    onClick={toggleMenu}
+                  >
+                    Liste Véhicule
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="/liste-categorie"
+                    className="text-white"
+                    onClick={toggleMenu}
+                  >
+                    Liste Catégorie
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="/ajout-vehicule-categorie"
+                    className="text-white"
+                    onClick={toggleMenu}
+                  >
+                    Ajout Véhicule
+                  </Link>
+                </li>
+              </>
+            )}
+          </ul>
+          <div className="absolute bottom-4 w-full flex justify-around items-center">
+            {!user.isAuthenticated ? (
+              <Link to="/form-auth" className="hover:text-gray-500">
+                <svg
+                  width="25px"
+                  height="25px"
+                  viewBox="0 0 16 16"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M8 7C9.65685 7 11 5.65685 11 4C11 2.34315 9.65685 1 8 1C6.34315 1 5 2.34315 5 4C5 5.65685 6.34315 7 8 7Z"
+                    fill="#FFFFFF"
+                  />
+                  <path
+                    d="M14 12C14 10.3431 12.6569 9 11 9H5C3.34315 9 2 10.3431 2 12V15H14V12Z"
+                    fill="#FFFFFF"
+                  />
+                </svg>
+              </Link>
+            ) : (
+              <button
+                onClick={logout}
+                className="hover:text-gray-500"
+                title="Déconnexion"
+              >
+                <svg
+                  viewBox="0 0 600 600"
+                  version="1.1"
+                  width="25"
+                  height="25"
+                >
+                        <path
+                    id="rect348"
+                    d="M 130 0 C 58.672245 0 0 58.672245 0 130 L 0 470 C 0 541.32776 58.672245 600 130 600 L 301.57812 600 C 367.83331 600 423.13643 549.36696 430.67188 485 L 349.43555 485 C 343.32179 505.66026 324.7036 520 301.57812 520 L 130 520 C 101.60826 520 80 498.39174 80 470 L 80 130 C 80 101.60826 101.60826 80 130 80 L 301.57812 80 C 324.7036 80 343.32179 94.339739 349.43555 115 L 430.67188 115 C 423.13642 50.633038 367.83331 0 301.57812 0 L 130 0 z"
+                  />
+                  <path
+                    id="path1073"
+                    d="m 476.86328,179.99911 a 40,40 0 0 0 -28.28516,11.71484 40,40 0 0 0 0,56.57032 l 11.71485,11.71484 H 163.72656 a 40,40 0 0 0 -40,40 40,40 0 0 0 40,40 h 296.56641 l -11.71485,11.71484 a 40,40 0 0 0 0,56.57032 40,40 0 0 0 56.57032,0 l 72.79101,-72.79102 A 40,40 0 0 0 600,299.99911 40,40 0 0 0 577.5293,264.09481 l -72.38086,-72.38086 a 40,40 0 0 0 -28.28516,-11.71484 z"
+                  />
+                </svg>
+              </button>
+            )}
+          </div>
+        </div>
+      )}
     </Fragment>
   );
 }
 
 export default Header;
+         
